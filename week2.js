fb9919b3e72b8f2031b9407c4d50ed01ab30982c
Kenny99T <kennytan421@gmail.com>
2022-03-22 11:14:13 +0800

    init

--- week2.js
+++ week2.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://m001-student:5BnK2tfSM1Vtl5Oh@sandbox.zy7nd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//Connecting to MongoDB server and print 'Connected to MongoDB' if connection success
 client.connect(err => {
     if (err) {
         console.log(err.message)
@@ -9,11 +10,38 @@ client.connect(err => {
     }
     console.log('Connected to MongoDB')
     
+    //Access to the databases and display all the databases
     // client.db().admin().listDatabases().then(result => {
     //     console.log(result);
     // })
 
-    client.db('sample_training').listCollections( ).toArray( ).then(result => {
+    //Access to the databases and display databases only
+    // client.db().admin().listDatabases().then(result => {
+    //     console.log(result['databases']);
+    // })
+
+    //Access to the databases and display database on index 7
+    // client.db().admin().listDatabases().then(result => {
+    //     console.log(result['databases'][7]);
+    // })
+
+    //List the collections inside the database
+    // client.db('sample_training').listCollections( ).toArray( ).then(result => {
+    //     console.log(result);
+    // })
+
+    //List the collections inside the database on index 4
+    // client.db('sample_training').listCollections( ).toArray( ).then(result => {
+    //     console.log(result[4]);
+    // })
+
+    //List all the document inside the collection
+    // client.db('sample_training').collection('zips').find().toArray().then(result => {
+    //     console.log(result);
+    // })
+
+    //Find the document that have been specified in find()
+    client.db('sample_training').collection('zips').find({city: 'BIRMINGHAM'}).toArray().then(result => {
         console.log(result);
     })
 });
