b605ebc9599d6d27124a8e9727e495364e199305
Kenny99T <kennytan421@gmail.com>
2022-03-28 21:39:31 +0800

    init

--- week3.js
+++ week3.js
@@ -1,44 +1,45 @@
+const bcrypt = require("bcryptjs")
 const { faker } = require('@faker-js/faker');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://m001-student:5BnK2tfSM1Vtl5Oh@sandbox.zy7nd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect( err => {
+client.connect( async err => {
     if (err) {
         console.log(err.message)
         return
     }
     console.log('Connected to MongoDB')
 
-    //Array to store all the names
-    let name = [];
-    for (let i = 0; i < 15; i++) {
-        const firstName = faker.name.firstName();
-        const lastName = faker.name.lastName();
-        name.push({ firstName, lastName });
+    const saltRounds = 10;
+    for(let i = 0; i < 50; i++){
+        const name = faker.name.findName();
+        const username = faker.internet.userName();
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+        let result = await client.db("Week3").collection("User").insertOne({
+            "Name": name, 
+            "Username": username,
+            "Email": email,
+        });
+        console.log(result);
+        console.log(password);
+        bcrypt.genSalt(saltRounds, function (saltError, salt) {
+            if (saltError) {
+                throw saltError
+            } 
+            else {
+                bcrypt.hash(password, salt, function(hashError, hash) {
+                    if (hashError) {
+                        throw hashError
+                    } 
+                    else {
+                        //console.log(hash);
+                        client.db("Week3").collection("User").updateOne({"Name": name},{"$set": {"Password": hash}});
+                    }
+                });
+            }
+        });
     }
-
-    //initial count of documents in database
-    client.db("new").collection("names").countDocuments().then(result => {
-        console.log(result);
-    });
-
-    //time to insert all the names
-    console.time('time');
-
-    //insert all the names into collection
-    client.db("new").collection("names").insertMany(name).then(result => {
-        console.log(result);
-    });
-
-    //time to insert all the names
-    console.timeEnd('time');
-
-    //count of documents in database after inserting all the names
-    client.db("new").collection("names").countDocuments().then(result => {
-        console.log(result);
-    });
-
-    //close the connection
     console.log('Completed');
 });
