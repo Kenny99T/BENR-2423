9001618026689fb1feac82832a14eb182eaf6d72
Kenny99T <kennytan421@gmail.com>
2022-05-16 19:55:41 +0800

    init

--- main.test.js
+++ main.test.js
@@ -20,10 +20,10 @@ describe('Lab 2', () => {
         expect(res.length).toBe(1);
         console.log(res);
         expect(res[0].accounts.length).toBe(4);
-        expect(res[0].accounts[0]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e4");
-        expect(res[0].accounts[1]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e6");
-        expect(res[0].accounts[2]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e3");
-        expect(res[0].accounts[3]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e5");
+        expect(res[0].accounts[0]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e3");
+        expect(res[0].accounts[1]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e4");
+        expect(res[0].accounts[2]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e5");
+        expect(res[0].accounts[3]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e6");
     });
 
     test('Test for Part A+B+C', async () => {
@@ -32,7 +32,7 @@ describe('Lab 2', () => {
         console.log(res);
         expect(res[0].accounts.length).toBe(3);
         expect(res[0].accounts[0]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e4");
-        expect(res[0].accounts[1]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e6");
-        expect(res[0].accounts[2]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e5");
+        expect(res[0].accounts[1]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e5");
+        expect(res[0].accounts[2]._id.toString()).toBe("5ca4bbc7a2dd94ee581625e6");
     });
 });
